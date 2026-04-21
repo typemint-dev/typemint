f4bb6d350e3abf5017d5720129e78843682bf7ad
Ales Albert KILBERGR <ales.albert@kilbergr.eu>
2026-04-21 18:45:50 +0200

    chore(workspace): add required test coverage levels

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,10 +4,32 @@ export default defineConfig({
   test: {
     include: ["packages/*/src/**/*.spec.ts", "**/*.spec.ts"],
     coverage: {
+      enabled: true,
       provider: "v8",
+      reportsDirectory: "./coverage/packages",
       include: ["packages/*/src/**/*.ts"],
-      exclude: ["**/*.spec.ts", "**/*.test.ts", "**/index.ts"],
-      reporter: ["text", "lcov"],
+      exclude: [
+        "**/index.ts",
+        "**/*.index.ts",
+        "**/*.spec.ts",
+        "**/*.test.ts",
+        "**/node_modules/**",
+      ],
+      reporter: ["text-summary", "text", "html", "lcov"],
+      thresholds: {
+        "packages/core/src/**": {
+          lines: 80,
+          functions: 80,
+          branches: 80,
+          statements: 80,
+        },
+        "packages/result/src/**": {
+          lines: 80,
+          functions: 80,
+          branches: 80,
+          statements: 80,
+        },
+      },
     },
   },
 });
