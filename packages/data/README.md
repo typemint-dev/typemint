35d12139df1abf3a1fbeefd8a7209248316bccab
Ales Albert KILBERGR <ales.albert@kilbergr.eu>
2026-04-23 08:49:22 +0200

    chore(data): init package

---
+++ packages/data/README.md
@@ -0,0 +1,7 @@
+# @typemint/data
+
+Type-safe data structures for TypeScript.
+
+## License
+
+MIT
---
+++ packages/data/src/index.spec.ts
@@ -0,0 +1,8 @@
+import { describe, expect, it } from 'vitest';
+import { Data } from './index.js';
+
+describe('Data', () => {
+  it('should be defined', () => {
+    expect(Data).toBeDefined();
+  });
+});
