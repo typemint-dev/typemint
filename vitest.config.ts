7e3cd069fb48db401ef08b4bca4db0c73167aa68
Ales Albert KILBERGR <ales.albert@kilbergr.eu>
2026-06-17 12:59:27 +0200

    test(data): setup test coverage expectation

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,33 +1,39 @@
-import { defineConfig } from "vitest/config";
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    include: ["packages/*/src/**/*.spec.ts", "**/*.spec.ts"],
+    include: ['packages/*/src/**/*.spec.ts', '**/*.spec.ts'],
     coverage: {
       enabled: true,
-      provider: "v8",
-      reportsDirectory: "./coverage/packages",
-      include: ["packages/*/src/**/*.ts"],
+      provider: 'v8',
+      reportsDirectory: './coverage/packages',
+      include: ['packages/*/src/**/*.ts'],
       exclude: [
-        "**/index.ts",
-        "**/*.index.ts",
-        "**/*.spec.ts",
-        "**/*.test.ts",
-        "**/node_modules/**",
+        '**/index.ts',
+        '**/*.index.ts',
+        '**/*.spec.ts',
+        '**/*.test.ts',
+        '**/node_modules/**',
       ],
-      reporter: ["text-summary", "text", "html", "lcov"],
+      reporter: ['text-summary', 'text', 'html', 'lcov'],
       thresholds: {
-        "packages/core/src/**": {
+        'packages/core/src/**': {
           lines: 80,
           functions: 80,
           branches: 80,
           statements: 80,
         },
-        "packages/result/src/**": {
+        'packages/result/src/**': {
           lines: 80,
           functions: 80,
           branches: 80,
           statements: 80,
+        },
+        'packages/data/src/**': {
+          lines: 100,
+          functions: 100,
+          branches: 100,
+          statements: 100,
         },
       },
     },
